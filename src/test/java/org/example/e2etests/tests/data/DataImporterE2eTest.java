00f7d6579d517c4ee66eb95be26adf803ac57953
1 <oleg2003andriy@gmail.com>
2026-07-05 20:46:48 +0200

    test: Добавлен e2e-тест импорта менторов и цен на гарантированные ревью

--- src/test/java/org/example/e2etests/tests/data/DataImporterE2eTest.java
+++ src/test/java/org/example/e2etests/tests/data/DataImporterE2eTest.java
@@ -27,6 +27,8 @@ public class DataImporterE2eTest extends E2eTestBase {
         jdbcTemplate.execute("TRUNCATE TABLE auth_service.users RESTART IDENTITY CASCADE");
         jdbcTemplate.execute("TRUNCATE TABLE profile_service.profiles RESTART IDENTITY CASCADE");
         jdbcTemplate.execute("TRUNCATE TABLE project_service.projects RESTART IDENTITY CASCADE");
+        jdbcTemplate.execute("TRUNCATE TABLE mentor_service.guaranteed_reviews_prices RESTART IDENTITY CASCADE");
+        jdbcTemplate.execute("TRUNCATE TABLE mentor_service.mentors RESTART IDENTITY CASCADE");
     }
 
     @Test
@@ -66,6 +68,31 @@ public class DataImporterE2eTest extends E2eTestBase {
         ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofSeconds(5));
         assertThat(records.count()).isGreaterThan(0);
         assertThat(records.iterator().next().value()).contains("author_telegram_user_id");
+    }
+
+    @Test
+    void shouldImportMentorsAndReviewPricesAndSendMessagesToKafka() throws InterruptedException {
+        assertTableIsEmpty("mentor_service.mentors");
+        assertTableIsEmpty("mentor_service.guaranteed_reviews_prices");
+
+        kafkaConsumer.subscribe(List.of("auth.user.created"));
+
+        startImport("/api/data-importer/start-users-import");
+        Thread.sleep(20_000);
+
+        startImport("/api/data-importer/start-profiles-import");
+        Thread.sleep(10_000);
+
+        startImport("/api/data-importer/start-guaranteed-reviews-import");
+
+        Thread.sleep(10_000);
+
+        assertTableHasRecords("mentor_service.mentors");
+        assertTableHasRecords("mentor_service.guaranteed_reviews_prices");
+
+        ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofSeconds(5));
+        assertThat(records.count()).isGreaterThan(0);
+        assertThat(records.iterator().next().value()).contains("telegram_user_id");
     }
 
     private void assertTableIsEmpty(String tableName) {
