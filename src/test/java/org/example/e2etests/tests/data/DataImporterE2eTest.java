fa74bbb48270caf9e4897b4ec9cb6bf32f797cf2
aleksLiss <darklissik.darklissik@gmail.com>
2026-09-08 23:30:34 +0300

    perf: изменена очистка таблиц - таблицы вынесены в Set TABLE_TO_TRUNCATE - очистка проводится через метод truncateTables в shouldImportMentorsAndReviewPricesAndSendMessagesToKafka добавлен импорт API_PROJECTS_IMPORT в shouldNotOverwriteMentorDescription добавлен импорт API_PROJECTS_IMPORT проверка, пуста ли таблица или содержит хотя бы одну запись вынесены в методы в E2eTestBase создание токена проводится через JwtTestUtils

--- src/test/java/org/example/e2etests/tests/data/DataImporterE2eTest.java
+++ src/test/java/org/example/e2etests/tests/data/DataImporterE2eTest.java
@@ -1,26 +1,22 @@
 package org.example.e2etests.tests.data;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.awaitility.core.ThrowingRunnable;
 import org.example.e2etests.tests.base.E2eTestBase;
+import org.example.e2etests.util.JwtTestUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpEntity;
-import org.springframework.test.jdbc.JdbcTestUtils;
 
-import javax.crypto.SecretKey;
 import java.time.Duration;
-import java.util.Base64;
-import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.awaitility.Awaitility.await;
-import static org.example.e2etests.HttpHeadersTestUtils.createHeaders;
+import static org.example.e2etests.util.HttpHeadersTestUtils.createHeaders;
 
 @Slf4j
 public class DataImporterE2eTest extends E2eTestBase {
@@ -33,26 +29,24 @@ public class DataImporterE2eTest extends E2eTestBase {
 
     private static final String UPDATE_DESCRIPTION_MENTOR_SQL = "UPDATE " + MENTOR_SERVICE_MENTOR_DESCRIPTIONS_TABLE + " SET cost = ? WHERE name = ?";
     private static final String GET_DESCRIPTION_MENTOR_SQL = "SELECT cost FROM " + MENTOR_SERVICE_MENTOR_DESCRIPTIONS_TABLE + " WHERE name = ?";
-
+    private static final Set<String> TABLE_TO_TRUNCATE = Set.of(
+            AUTH_SERVICE_USERS_TABLE,
+            PROFILE_SERVICE_PROFILES_TABLE,
+            PROJECT_SERVICE_PROJECTS_TABLE,
+            MENTOR_SERVICE_GUARANTEED_REVIEWS_PRICES_TABLE,
+            MENTOR_SERVICE_MENTORS_TABLE,
+            PROFILE_SERVICE_PROJECT_TABLE
+    );
 
     @BeforeEach
     void setUp() {
-        jdbcTemplate.execute("TRUNCATE TABLE auth_service.users RESTART IDENTITY CASCADE");
-        jdbcTemplate.execute("TRUNCATE TABLE profile_service.profiles RESTART IDENTITY CASCADE");
-        jdbcTemplate.execute("TRUNCATE TABLE project_service.projects RESTART IDENTITY CASCADE");
-        jdbcTemplate.execute("TRUNCATE TABLE mentor_service.guaranteed_reviews_prices RESTART IDENTITY CASCADE");
-        jdbcTemplate.execute("TRUNCATE TABLE mentor_service.mentors RESTART IDENTITY CASCADE");
+        truncateTables(TABLE_TO_TRUNCATE);
     }
 
     @AfterEach
     void cleanTables() {
-        jdbcTemplate.execute("TRUNCATE TABLE auth_service.users RESTART IDENTITY CASCADE");
-        jdbcTemplate.execute("TRUNCATE TABLE profile_service.profiles RESTART IDENTITY CASCADE");
-        jdbcTemplate.execute("TRUNCATE TABLE project_service.projects RESTART IDENTITY CASCADE");
-        jdbcTemplate.execute("TRUNCATE TABLE mentor_service.guaranteed_reviews_prices RESTART IDENTITY CASCADE");
-        jdbcTemplate.execute("TRUNCATE TABLE mentor_service.mentors RESTART IDENTITY CASCADE");
+        truncateTables(TABLE_TO_TRUNCATE);
     }
-
 
     @Test
     void shouldImportUsersAndSendMessagesToKafka() {
@@ -109,9 +103,13 @@ public class DataImporterE2eTest extends E2eTestBase {
         startImport(API_PROFILES_IMPORT);
         executeWithAwait(60,
                 () -> {
-                    assertTableHasRecords(PROFILE_SERVICE_PROJECT_TABLE);
                     assertTableHasRecords(PROFILE_SERVICE_PROFILES_TABLE);
                 }
+        );
+
+        startImport(API_PROJECTS_IMPORT);
+        executeWithAwait(60, () ->
+                assertTableHasRecords(PROFILE_SERVICE_PROJECT_TABLE)
         );
 
         startImport(API_MENTORS_IMPORT);
@@ -130,7 +128,6 @@ public class DataImporterE2eTest extends E2eTestBase {
                 assertTableHasRecords(MENTOR_SERVICE_GUARANTEED_REVIEWS_PRICES_TABLE)
         );
 
-
         executeWithAwait(60, () -> {
             ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofSeconds(1));
             assertThat(records.count()).isGreaterThan(0);
@@ -146,9 +143,13 @@ public class DataImporterE2eTest extends E2eTestBase {
         startImport(API_PROFILES_IMPORT);
         executeWithAwait(60,
                 () -> {
-                    assertTableHasRecords(PROFILE_SERVICE_PROJECT_TABLE);
                     assertTableHasRecords(PROFILE_SERVICE_PROFILES_TABLE);
                 }
+        );
+
+        startImport(API_PROJECTS_IMPORT);
+        executeWithAwait(60, () ->
+                assertTableHasRecords(PROFILE_SERVICE_PROJECT_TABLE)
         );
 
         startImport(API_MENTORS_IMPORT);
@@ -184,47 +185,20 @@ public class DataImporterE2eTest extends E2eTestBase {
                 GET_DESCRIPTION_MENTOR_SQL,
                 String.class, testName
         );
-
         assertThat(descriptionAfter).isEqualTo(description);
-
     }
 
-
     private void executeWithAwait(int second, ThrowingRunnable throwingRunnable) {
-
         await()
                 .atMost(Duration.ofSeconds(second))
                 .pollInterval(Duration.ofSeconds(3))
                 .untilAsserted(throwingRunnable);
     }
 
-    private void assertTableIsEmpty(String tableName) {
-        int count = JdbcTestUtils.countRowsInTable(jdbcTemplate, tableName);
-        assertThat(count).isZero();
-    }
-
-    private String createAdminToken() {
-        SecretKey key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtSecret));
-        Date now = new Date();
-        return Jwts.builder()
-                .subject("123456789")
-                .claim("roles", List.of("ADMIN"))
-                .claim("telegram_username", "e2e_test_admin")
-                .issuedAt(now)
-                .expiration(new Date(now.getTime() + 3_600_000))
-                .signWith(key)
-                .compact();
-    }
-
-    private void assertTableHasRecords(String tableName) {
-        int count = JdbcTestUtils.countRowsInTable(jdbcTemplate, tableName);
-        assertThat(count).isGreaterThan(0);
-    }
-
     private void startImport(String path) {
         testRestTemplate.postForEntity(
                 path,
-                new HttpEntity<>(createHeaders(createAdminToken())),
+                new HttpEntity<>(createHeaders(JwtTestUtils.getAdminJWT(jwtSecret))),
                 String.class
         );
     }
